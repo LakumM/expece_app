ccac3fc0049e64e9b20b004dfee82e131085e65a
RRComputer <mukeshlakum79@gmail.com>
2024-05-02 19:01:26 +0530

    New Changes Apply dt 02-05-24 time 18:59

---
+++ lib/Data/model/user_model.dart
@@ -0,0 +1,14 @@
+class User_Model {
+  int uID;
+  String uName;
+  String uEmail;
+  String uMobile;
+  String uPass;
+
+  User_Model(
+      {required this.uID,
+      required this.uName,
+      required this.uEmail,
+      required this.uMobile,
+      required this.uPass});
+}
